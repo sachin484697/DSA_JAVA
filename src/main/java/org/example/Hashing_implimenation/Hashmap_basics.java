b8127c83babdf60819590c55ac0630e249c1b7d9
sachin484697 <phapalesachin@107gamil.com>
2025-03-18 21:32:05 +0530

    tree class is added

---
+++ src/main/java/org/example/Hashing_implimenation/Hashmap_basics.java
@@ -0,0 +1,41 @@
+package org.example.Hashing_implimenation;
+
+import java.util.HashMap;
+
+public class Hashmap_basics {
+
+
+    // A HashMap internally uses an array of Node<K, V> objects, where each Node represents an entry in the map.
+
+
+     public static void main(String[] args) {
+
+
+
+                HashMap<Integer, String> map = new HashMap<>();
+
+                // Insert elements
+                map.put(1, "Apple");
+                map.put(2, "Banana");
+                map.put(3, "Cherry");
+
+                // Retrieve element
+                System.out.println("Value for key 2: " + map.get(2));
+
+                // Remove an element
+                map.remove(1);
+
+                // Iterate over entries
+                for (var entry : map.entrySet()) {
+                    System.out.println(entry.getKey() + " -> " + entry.getValue());
+                }
+            }
+        }
+
+
+
+
+
+
+
+
---
+++ src/main/java/org/example/tree_implimentation/tree_basics.java
@@ -0,0 +1,8 @@
+package org.example.tree_implimentation;
+
+public class tree_basics {
+
+    public static void main(String[] args) {
+
+    }
+}
